1654702c76a1f1952a246f14fa410c2911f20710
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-09-09 11:47:14 -0300

    hotFix- routes added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import './App.css';
 import UserElections from './UserElections/UserElections';
-import Election from './UserElections/Election/UserElection';
+import UserElection from './UserElections/Election/UserElection';
 import Home from './Pages/Home/JSX/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Election from './Pages/Election/JSX/Election';
+import PageTableInfo from './Pages/TableInfo/JSX/PageTableInfo';
 
 function App() {
   return (
@@ -11,7 +13,9 @@ function App() {
       <Routes>
         <Route path="/home" element={<Home/> } />
         <Route path="/" element={<UserElections/>} />
-        <Route path="/userElections/election" element={<Election />} />
+        <Route path="/userElections/election" element={<UserElection />} />
+        <Route path="/election" element={<Election/>} />
+        <Route path="/election/table" element={<PageTableInfo/>} />
       </Routes>
     </Router>
   );
