2a17d8fdeff0a71ad4dd58488b3f8695e2546a3e
NikyySalama <nikysalama0@example.com>
2024-09-09 12:02:10 -0300

    hotfix: missing dependencies for carousel

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home/> } />
-        <Route path="/" element={<UserElections/>} />
+        <Route path="/" element={<Home/> } />
+        <Route path="/userElections" element={<UserElections/>} />
         <Route path="/userElections/election" element={<UserElection />} />
         <Route path="/election" element={<Election/>} />
         <Route path="/election/table" element={<PageTableInfo/>} />
