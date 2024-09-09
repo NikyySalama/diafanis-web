a769c060b482a6ae1342cd9900e9b2076aff49f9
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-09-09 11:36:05 -0300

    hotFix-common context NO MERGE

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import './App.css';
-import UserElections from './UserElections';
+
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HeadContent from './Pages/Home/JSX/HeadContent';
+import { ClassNames } from '@emotion/react';
+import Home from './Pages/Home/JSX/Home';
+//import './index.css';
+import Election from './Pages/Election/JSX/Election';
+
+import PageTableInfo from './Pages/TableInfo/JSX/PageTableInfo';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<UserElections/> } />
+        <Route path="/"  ClassNames='main' element={<Home/> } />
+        <Route path="/election" element={<Election/>} />
+        <Route path="/election/table" element={<PageTableInfo/>} />
       </Routes>
     </Router>
   );
