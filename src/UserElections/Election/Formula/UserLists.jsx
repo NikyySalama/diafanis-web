c19409fe3a4748e8830c9f38500c9751fb4db4d6
NikyySalama <nikysalama0@example.com>
2024-09-28 15:46:09 -0300

    feat: formulas id and party editable

--- src/UserElections/Election/Formula/UserLists.jsx
+++ src/UserElections/Election/Formula/UserLists.jsx
@@ -13,29 +13,27 @@ const UserLists = () => {
   const [showModal, setShowModal] = useState(false);
   const [showPositionsModal, setShowPositionsModal] = useState(false);
   const [showModalFormula, setShowModalFormula] = useState(false);
-  const [formData, setFormData] = useState({ partyId: '', partyName: '', id: '' });
+  const [formData, setFormData] = useState({ partyUuid: '', partyName: '', id: '' });
   const [positionsData, setPositionsData] = useState([]);
-  const [clickedFormula, setClickedFormula] = useState(false);
   const [editFormulaData, setEditFormulaData] = useState({
-    formulaNumber: '', 
-    partyId: '', 
-    partyName: '', 
+    idNumber: '', 
+    partyUuid: '', 
     candidates: [],
     uuid: ''
   });
 
   useEffect(() => {
-    const fetchData = async () => {
-      const fetchedFormulas = await fetchFormulas();
-      const fetchedPositions = await fetchPositions();
-
-      setFormulas(fetchedFormulas);
-      setPositions(fetchedPositions);
-      setPositionsData(new Array(fetchedPositions.length).fill(null));
-    };
     fetchData();
     fetchParties();
   }, []);
+
+  const fetchData = async () => {
+    const [fetchedFormulas, fetchedPositions] = await Promise.all([fetchFormulas(), fetchPositions()]);
+    console.log("formulas: ", fetchedFormulas);
+    setFormulas(fetchedFormulas);
+    setPositions(fetchedPositions);
+    setPositionsData(new Array(fetchedPositions.length).fill(null));
+  };
 
   const fetchParties = async () => {
     try {
@@ -56,16 +54,16 @@ const UserLists = () => {
       const response = await fetch(`http://localhost:8080/api/elections/${electionId}/electionPositions`);
       if (response.ok) {
         const positions = await response.json();
-        const formulas = positions.flatMap(position =>
+        return positions.flatMap(position => 
           position.formulas.map(formula => ({
             title: position.title,
-            formulaNumber: formula.idNumber,
+            idNumber: formula.idNumber,
+            partyUuid: formula.party.uuid, // Asegúrate de usar `uuid` correctamente
             partyName: formula.party.name,
             candidates: formula.candidates,
-            uuid: formula.uuid, // Añadimos el UUID de la fórmula para poder editarla
+            uuid: formula.uuid,
           }))
         );
-        return formulas;
       } else {
         throw new Error('Error al obtener las posiciones');
       }
@@ -78,11 +76,7 @@ const UserLists = () => {
   const fetchPositions = async () => {
     try {
       const response = await fetch(`http://localhost:8080/api/elections/${electionId}/electionPositions`);
-      if (response.ok) {
-        return await response.json();
-      } else {
-        throw new Error('Error al obtener las posiciones');
-      }
+      return response.ok ? await response.json() : [];
     } catch (error) {
       console.error('Error en fetchPositions:', error);
       return [];
@@ -90,7 +84,7 @@ const UserLists = () => {
   };
 
   const handleCreateListClick = () => {
-    setFormData({ partyId: '', partyName: '', id: '' });
+    setFormData({ partyUuid: '', partyName: '', id: '' });
     setShowModal(true);
   };
 
@@ -101,9 +95,9 @@ const UserLists = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name === 'partyId') {
+    if (name === 'partyUuid') {
       const selectedParty = parties.find(party => party.uuid === value);
-      setFormData({ ...formData, partyId: value, partyName: selectedParty ? selectedParty.name : '' });
+      setFormData({ ...formData, partyUuid: value, partyName: selectedParty?.name || '' });
     } else {
       setFormData({ ...formData, [name]: value });
     }
@@ -118,35 +112,29 @@ const UserLists = () => {
   const handleFileUpload = (e, positionId) => {
     const file = e.target.files[0];
     const reader = new FileReader();
-  
+
     reader.onload = (event) => {
       const binaryStr = event.target.result;
       const workbook = XLSX.read(binaryStr, { type: 'array' });
       const sheet = workbook.Sheets[workbook.SheetNames[0]];
       const data = XLSX.utils.sheet_to_json(sheet);
-  
+
       const requiredColumns = ['DNI', 'name', 'lastName', 'imageUrl', 'role'];
       const fileColumns = Object.keys(data[0] || {});
-  
-      const isValidStructure = requiredColumns.every(col => fileColumns.includes(col));
-      if (!isValidStructure) {
+
+      if (!requiredColumns.every(col => fileColumns.includes(col))) {
         alert('El archivo debe contener las columnas: DNI, name, lastName, imageUrl, role.');
         e.target.value = '';
         return;
       }
-  
+
       const dniRegex = /^[0-9]+$/;
-      const hasValidDNI = data.every(row => {
-        const dniValue = row.DNI;
-        return typeof dniValue === 'number' || (typeof dniValue === 'string' && dniRegex.test(dniValue));
-      });
-  
-      if (!hasValidDNI) {
+      if (!data.every(row => dniRegex.test(row.DNI))) {
         alert('El campo DNI debe contener solo valores numéricos.');
         e.target.value = '';
         return;
       }
-  
+
       const candidates = data.map(row => ({
         role: row.role || 'Desconocido',
         docNumber: parseInt(row.DNI, 10) || 0,
@@ -155,30 +143,28 @@ const UserLists = () => {
         surname: row.lastName || 'Apellido Desconocido',
         image: row.imageUrl || ''
       }));
-  
+
       setPositionsData(prevData => {
         const updatedData = [...prevData];
         updatedData[positionId] = candidates;
         return updatedData;
       });
     };
-  
+
     reader.readAsArrayBuffer(file);
-  };  
+  };
 
   const handlePositionsSubmit = async (e) => {
     e.preventDefault();
 
-    const allFilesUploaded = positions.every((_, index) => positionsData[index] && positionsData[index].length > 0);
-  
-    if (!allFilesUploaded) {
+    if (positions.some((_, index) => !positionsData[index]?.length)) {
       alert('Por favor, suba un archivo para cada posición.');
       return;
     }
 
     try {
-      const postPromises = positions.map(async (position, index) => {
-        const candidates = positionsData[index]?.map((candidate, candidateIndex) => ({
+      const postPromises = positions.map((position, index) => {
+        const candidates = positionsData[index].map((candidate, candidateIndex) => ({
           role: candidate.role,
           imageUrl: candidate.image,
           zindex: candidateIndex,
@@ -189,54 +175,50 @@ const UserLists = () => {
             lastName: candidate.surname,
             imageUrl: candidate.image
           },
-        })) || [];
+        }));
 
         const formulaData = {
           title: 'title',
-          partyUuid: formData.partyId,
+          partyUuid: formData.partyUuid,
           idNumber: formData.id,
           candidates,
         };
 
-        const response = await fetch(`http://localhost:8080/api/electiveFormulas/${position.uuid}`, {
+        return fetch(`http://localhost:8080/api/electiveFormulas/${position.uuid}`, {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(formulaData),
         });
-
-        if (!response.ok) throw new Error(`Error al enviar la fórmula para ${position.title}`);
-        return response.json();
       });
 
-      const results = await Promise.all(postPromises);
+      await Promise.all(postPromises);
+      setShowPositionsModal(false);
     } catch (error) {
       console.error('Error en el envío de fórmulas:', error);
     }
-
-    setShowPositionsModal(false);
   };
 
-  // Envía los datos de la fórmula editada
+  //TODO: change formula data
+
   const handleUpdateFormula = async (e) => {
     e.preventDefault();
-    if (!editFormulaData || !editFormulaData.uuid || !editFormulaData.partyId) {
-      console.error('Datos incompletos o inválidos:', editFormulaData);
-      return;
+    if (!editFormulaData.uuid || !editFormulaData.partyUuid) return;
+
+    const formulaDataToPatch = {
+      idNumber: editFormulaData.idNumber,
+      partyUuid: editFormulaData.partyUuid,
     }
 
     try {
       const response = await fetch(`http://localhost:8080/api/electiveFormulas/${editFormulaData.uuid}`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(editFormulaData),
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(formulaDataToPatch),
       });
-      console.log("formula data: ", editFormulaData);
 
       if (response.ok) {
-        const savedFormula = await response.json();
-        setFormulas(formulas.map(f => (f.uuid === savedFormula.uuid ? savedFormula : f))); // Actualiza las fórmulas
+        const updatedFormula = await response.json();
+        setFormulas(formulas.map(f => f.uuid === updatedFormula.uuid ? updatedFormula : f));
         setShowModalFormula(false);
       } else {
         console.error('Error al actualizar la fórmula:', response.statusText);
@@ -244,19 +226,24 @@ const UserLists = () => {
     } catch (error) {
       console.error('Error en la solicitud:', error);
     }
+    console.log("formula subida: ", formulaDataToPatch);
   };
 
   const columns = [
     { label: 'Posición', field: 'title' },
     { label: 'Partido', field: 'partyName' },
-    { label: 'ID', field: 'formulaNumber' },
+    { label: 'ID', field: 'idNumber' },
   ];
 
   return (
     <div className="my-section">
-      <div className="my-section-header"></div>
-      
-      <CustomTable title="Sus Fórmulas" columns={columns} rows={formulas} onRowClick={(row) => handleEditFormulaClick(row)} handleAddSelected={handleCreateListClick} />
+      <CustomTable 
+        title="Sus Fórmulas" 
+        columns={columns} 
+        rows={formulas} 
+        onRowClick={handleEditFormulaClick} 
+        handleAddSelected={handleCreateListClick} 
+      />
 
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
@@ -277,131 +264,87 @@ const UserLists = () => {
               />
             </div>
             <div className="form-group">
-              <label htmlFor="partyId">Elegir partido:</label>
+              <label htmlFor="partyUuid">Partido:</label>
               <select
-                id="partyId"
-                name="partyId"
-                value={formData.partyId}
+                id="partyUuid"
+                name="partyUuid"
+                value={formData.partyUuid}
                 onChange={handleChange}
                 required
               >
-                <option value="">Seleccione un partido</option>
-                {parties.map(party => (
-                  <option key={party.uuid} value={party.uuid}>{party.name}</option>
+                <option value="">Seleccionar Partido</option>
+                {parties.map((party) => (
+                  <option key={party.uuid} value={party.uuid}>
+                    {party.name}
+                  </option>
                 ))}
               </select>
             </div>
-            <button type="submit" className="modal-button">Siguiente</button>
+            <button type="submit" className="btn btn-primary">Crear Lista</button>
           </form>
         </Modal.Body>
       </Modal>
 
       <Modal show={showPositionsModal} onHide={() => setShowPositionsModal(false)}>
         <Modal.Header closeButton>
-          <Modal.Title>Ingrese los Candidatos de las Posiciones</Modal.Title>
+          <Modal.Title>Posiciones y Candidatos</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {positions.map((position, index) => (
-            <div key={index} className="form-group">
-              <h5>{position.title}</h5>
-              <label>Subir archivo Excel para {position.title}:</label>
-              <input
-                type="file"
-                accept=".xlsx, .xls"
-                onChange={e => handleFileUpload(e, index)}
-                required
-              />
-            </div>
-          ))}
-          <button type="submit" className="modal-button" onClick={handlePositionsSubmit}>Guardar</button>
+          <form onSubmit={handlePositionsSubmit}>
+            {positions.map((position, index) => (
+              <div key={position.uuid} className="form-group">
+                <label htmlFor={`position${index}`}>{position.title}</label>
+                <input
+                  type="file"
+                  id={`position${index}`}
+                  name={`position${index}`}
+                  accept=".xlsx, .xls"
+                  onChange={(e) => handleFileUpload(e, index)}
+                  required
+                />
+              </div>
+            ))}
+            <button type="submit" className="btn btn-primary">Subir Fórmulas</button>
+          </form>
         </Modal.Body>
       </Modal>
 
-      {/* Modal para editar una fórmula */}
       <Modal show={showModalFormula} onHide={() => setShowModalFormula(false)}>
         <Modal.Header closeButton>
           <Modal.Title>Editar Fórmula</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {editFormulaData && (
-            <form onSubmit={handleUpdateFormula}>
-              <div className="form-group">
-                <label htmlFor="formulaNumber">ID de fórmula:</label>
-                <input
-                  type="text"
-                  id="formulaNumber"
-                  name="formulaNumber"
-                  value={editFormulaData.formulaNumber || ''}
-                  onChange={(e) =>
-                    setEditFormulaData({ ...editFormulaData, formulaNumber: e.target.value })
-                  }
-                  required
-                />
-              </div>
-              <div className="form-group">
-                <label htmlFor="partyName">Partido:</label>
-                <select
-                  id="partyId"
-                  name="partyId"
-                  value={editFormulaData.partyId || ''}
-                  onChange={(e) => {
-                    const selectedParty = parties.find(party => party.uuid === e.target.value);
-                    setEditFormulaData({
-                      ...editFormulaData,
-                      partyId: e.target.value,
-                      partyName: selectedParty ? selectedParty.name : ''
-                    });
-                  }}
-                  required >
-                  <option value="">Seleccione un partido</option>
-                  {parties.map(party => (
-                    <option key={party.uuid} value={party.uuid}>{party.name}</option>
-                  ))}
-                </select>
-              </div>
-              <div className="form-group">
-                <label>Candidatos:</label>
-                {editFormulaData.candidates.map((candidate, index) => (
-                  <div key={index}>
-                    <input
-                      type="text"
-                      value={candidate.data.name || ''}
-                      onChange={(e) => {
-                        const updatedCandidates = [...editFormulaData.candidates];
-                        updatedCandidates[index].data.name = e.target.value;
-                        setEditFormulaData({ ...editFormulaData, candidates: updatedCandidates });
-                      }}
-                      placeholder="Nombre"
-                      required
-                    />
-                    <input
-                      type="text"
-                      value={candidate.data.lastName || ''}
-                      onChange={(e) => {
-                        const updatedCandidates = [...editFormulaData.candidates];
-                        updatedCandidates[index].data.lastName = e.target.value;
-                        setEditFormulaData({ ...editFormulaData, candidates: updatedCandidates });
-                      }}
-                      placeholder="Apellido"
-                      required
-                    />
-                    <input
-                      type="text"
-                      value={candidate.role || ''}
-                      onChange={(e) => {
-                        const updatedCandidates = [...editFormulaData.candidates];
-                        updatedCandidates[index].role = e.target.value;
-                        setEditFormulaData({ ...editFormulaData, candidates: updatedCandidates });
-                      }}
-                      placeholder="Rol"
-                      required
-                    />
-                  </div>
+          <form onSubmit={handleUpdateFormula}>
+            <div className="form-group">
+              <label htmlFor="id">ID de fórmula:</label>
+              <input
+                type="text"
+                id="id"
+                name="idNumber"
+                value={editFormulaData.idNumber}
+                onChange={(e) => setEditFormulaData({ ...editFormulaData, idNumber: e.target.value })}
+                required
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="partyUuid">Partido:</label>
+              <select
+                id="partyUuid"
+                name="partyUuid"
+                value={editFormulaData.partyUuid}
+                onChange={(e) => setEditFormulaData({ ...editFormulaData, partyUuid: e.target.value })}
+                required
+              >
+                <option value="">Seleccionar Partido</option>
+                {parties.map((party) => (
+                  <option key={party.uuid} value={party.uuid}>
+                    {party.name}
+                  </option>
                 ))}
-              </div>
-              <button type="submit" className="modal-button">Guardar Cambios</button>
-            </form>
-          )}
+              </select>
+            </div>
+            <button type="submit" className="btn btn-primary">Guardar Cambios</button>
+          </form>
         </Modal.Body>
       </Modal>
     </div>
