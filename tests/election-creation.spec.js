41c56229233f2f45c730881055d9e768b3590516
NikyySalama <nikysalama0@example.com>
2024-10-14 13:21:52 -0300

    fix: test form election creation

--- tests/election-creation.spec.js
+++ tests/election-creation.spec.js
@@ -1,23 +1,39 @@
 const { test, expect } = require('@playwright/test');
 
-// Cargar el estado de sesión al inicio
-test.use({ storageState: 'storageState.json' });
+let context;
+let page;
+
+test.beforeAll(async ({ browser }) => {
+  context = await browser.newContext();
+  page = await context.newPage();
+
+  await page.goto('http://localhost:3000');
+  await page.evaluate(() => {
+    sessionStorage.setItem('jwt', 'token');
+    sessionStorage.setItem('user', 'Nicole');
+  });
+});
+
+test.afterAll(async () => {
+  await context.close();
+});
 
 test.describe('Testing Form', () => {
-  test.beforeEach(async ({ page }) => {
+  test.beforeEach(async () => {
     await page.goto('http://localhost:3000/userElections');
-    await page.click('button:has(svg)');  // Click en el botón con ícono
+    await page.waitForSelector('button[aria-label="Crear Eleccion"]'); // Ensure the button is loaded
+    await page.click('button[aria-label="Crear Eleccion"]');
   });
 
-  test('Debería mostrar una alerta si las fechas son incorrectas', async ({ page }) => {
+  test('Debería mostrar una alerta si las fechas son incorrectas', async () => {
     page.on('dialog', async (dialog) => {
       expect(dialog.message()).toBe('La fecha de fin debe ser posterior a la fecha de inicio.');
       await dialog.dismiss();
     });
 
-    await page.fill('input[placeholder="Título"]', 'Elección Test');
-    await page.fill('input[type="datetime-local"]:nth-of-type(1)', '2024-10-14T10:00');
-    await page.fill('input[type="datetime-local"]:nth-of-type(2)', '2024-10-14T09:00'); // Fecha incorrecta
+    await page.fill('#title', 'Elección Test');
+    await page.fill('#startsAt', '2024-10-14T10:00');
+    await page.fill('#endsAt', '2024-10-14T09:00');
 
     await page.click('button[type="submit"]');
   });
