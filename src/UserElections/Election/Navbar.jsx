6aede02db38a211b86fc3e7877fd351f84f16507
NikyySalama <nikysalama0@example.com>
2024-11-02 21:58:14 -0300

    feat: navbar updated colors

--- src/UserElections/Election/Navbar.jsx
+++ src/UserElections/Election/Navbar.jsx
@@ -4,34 +4,41 @@ import { CssBaseline } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({ setActiveSection, title }) => {
+  const [activeItem, setActiveItem] = useState('info'); // Estado para el elemento seleccionado
 
   const handleGoBack = () => {
     window.history.back();
   };
   const navigate = useNavigate();
   useEffect(() => {
-    const isLoggedIn = sessionStorage.getItem('jwt'); // Example check
+    const isLoggedIn = sessionStorage.getItem('jwt');
     if (!isLoggedIn) {
-      navigate('/'); // Redirect to login if not authenticated
+      navigate('/');
     }
-  }, [navigate]); // Ensure that `navigate` is included in the dependency array
+  }, [navigate]);
+
+  // Manejador para cambiar el item activo
+  const handleItemClick = (section) => {
+    setActiveSection(section);
+    setActiveItem(section);
+  };
 
   return (
     <>
       <CssBaseline />
       <nav className="navbar">
-          <div className='navbar-title-section'>
-            <button className="go-back-button" onClick={handleGoBack}>Volver</button>
-              <p className="navbar-title">{title}</p>
-          </div>
-          <ul className="nav-items">
-              <li className="nav-item" onClick={() => setActiveSection('info')}>Info</li>
-              <li className="nav-item" onClick={() => setActiveSection('parties')}>Partidos</li>
-              <li className="nav-item" onClick={() => setActiveSection('lists')}>Formulas</li>
-              <li className="nav-item" onClick={() => setActiveSection('tables')}>Mesas</li>
-              <li className="nav-item" onClick={() => setActiveSection('voters')}>Votantes</li>
-              <li className="nav-item" onClick={() => setActiveSection('authorities')}>Autoridades</li>
-          </ul>
+        <div className='navbar-title-section'>
+          <button className="go-back-button" onClick={handleGoBack}>Volver</button>
+          <p className="navbar-title">{title}</p>
+        </div>
+        <ul className="nav-items">
+          <li className={`nav-item ${activeItem === 'info' ? 'active' : ''}`} onClick={() => handleItemClick('info')}>Info</li>
+          <li className={`nav-item ${activeItem === 'parties' ? 'active' : ''}`} onClick={() => handleItemClick('parties')}>Partidos</li>
+          <li className={`nav-item ${activeItem === 'lists' ? 'active' : ''}`} onClick={() => handleItemClick('lists')}>Formulas</li>
+          <li className={`nav-item ${activeItem === 'tables' ? 'active' : ''}`} onClick={() => handleItemClick('tables')}>Mesas</li>
+          <li className={`nav-item ${activeItem === 'voters' ? 'active' : ''}`} onClick={() => handleItemClick('voters')}>Votantes</li>
+          <li className={`nav-item ${activeItem === 'authorities' ? 'active' : ''}`} onClick={() => handleItemClick('authorities')}>Autoridades</li>
+        </ul>
       </nav>
     </>
   );
