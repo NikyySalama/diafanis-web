cfb674f57b997387c3679b8e6be394c4b693f65c
Borda Tomas <80111893+Bordita@users.noreply.github.com>
2024-12-15 19:07:08 -0300

    Update MainContent.jsx

--- src/Pages/Election/JSX/MainContent.jsx
+++ src/Pages/Election/JSX/MainContent.jsx
@@ -25,7 +25,7 @@ const CarouselComponent = ({ results, positions, formulaMap, display, totalVotes
               {position.title || 'Unknown Position'}
             </Typography>
             <Typography variant="body1" sx={{ textAlign: 'center', marginTop: '0.5em' }}>
-              Total Votes: {people - totalVotes[positionId] || 0}
+              Votos en blanco: {people - totalVotes[positionId] || 0}
             </Typography>
             <Box sx={{ overflowY: 'auto', padding: '1em', display: 'flex', flexDirection: 'column', flexGrow: 1, width: '100%' }}>
               {display && results && results[positionId] ? (
